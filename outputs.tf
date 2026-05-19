7186738661cb996e5b58e8be983f4a5816191ae5
defnotyujine <batauling1000@gmail.com>
2026-05-19 09:45:53 +0800

    Fixed outputs.tf to handle empty collections

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+# output "default-vm_ips" {
+    # value = libvirt_domain.rhel9[*].network_interface[0].addresses[0]
+# }
+
 output "default-vm_ips" {
-    value = libvirt_domain.rhel9[*].network_interface[0].addresses[0]
+  value = length(libvirt_domain.rhel9) > 0 ? libvirt_domain.rhel9[*].network_interface[0].addresses[0] : []
 }
